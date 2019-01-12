db7d8ded7dcebb3dd01415ea090d01c199797187
Sebastian Balaj <darius.balaj@icloud.com>
2019-01-13 00:43:02 +0200

    voucher

--- script/script.js
+++ script/script.js
@@ -1,3 +1,25 @@
 $(document).ready(function(){
-	$('.parallax').parallax();
+
+	if($('.parallax').length > 0){
+		$('.parallax').parallax();
+	}
+	
+	$(".type-wrap").typed({
+  strings: ["website?", "logo?", "orice alt design?"],
+  stringsElement: null,
+  typeSpeed: 0,
+  startDelay: 0,
+  backSpeed: 0,
+  shuffle: false,
+  backDelay: 2000,
+  fadeOut: false,
+  fadeOutClass: 'typed-fade-out',
+  fadeOutDelay: 1500,
+  loop: true,
+  loopCount: false,
+  showCursor: true,
+  cursorChar: "|",
+  attr: null,
+  contentType: 'html'
+});
 })
