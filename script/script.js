3d0b0afcdbbf5aea49993d3e0ad64be635391272
Balaj Sebastian <darius.balaj@icloud.com>
2019-08-21 11:12:03 +0300

    .

--- script/script.js
+++ script/script.js
@@ -1,15 +1,20 @@
   $(document).ready(function(){
-    $('.projects-menu > a').each(function() {
+    $('.portfolio-content').each(function() {
       $(this).click(function(e) {
         e.preventDefault();
         var link = $(this).attr('rel');
-        $('.active-event').removeClass('active-event');
-        $(this).addClass('active-event');
-        $('.portfolio-content').hide();
-        $('div#' + link).fadeIn();
+        $('.sidebar').removeClass('active-sidebar');
+        $('.custom-popup').fadeIn();
+        $('html').addClass('shown-sidebar');
+        $('div#' + link).addClass('active-sidebar');
       });
-      $('#dynamic_form_639 .btn').attr('value', 'SUBSCRIBE');
     });
+    $('.custom-popup, .close-popup').click(function(e){
+      e.preventDefault();
+      $('.custom-popup').fadeOut();
+      $('html').removeClass('shown-sidebar');
+        $('.sidebar').removeClass('active-sidebar');
+    })
     if($('.testimonials-slider').length > 0){
       $('.testimonials-slider').flexslider({
         animation: 'slide'
@@ -18,25 +23,6 @@
     if($('.parallax').length > 0){
       $('.parallax').parallax();
     }
-    if($('.type-wrap').length > 0){
-      $(".type-wrap").typed({
-        strings: ["website?", "logo?", "orice alt design?"],
-        stringsElement: null,
-        typeSpeed: 0,
-        startDelay: 0,
-        backSpeed: 0,
-        shuffle: false,
-        backDelay: 2000,
-        fadeOut: false,
-        fadeOutClass: 'typed-fade-out',
-        fadeOutDelay: 1500,
-        loop: true,
-        loopCount: false,
-        showCursor: true,
-        cursorChar: "|",
-        attr: null,
-        contentType: 'html'
-      });
-    }
+
 
   })
